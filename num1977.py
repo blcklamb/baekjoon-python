952ac784bbb70481199c2233720e6525e9a90f61
[4기-1반] 김채정 <whenucan35@gmail.com>
2022-04-25 13:54:13 +0900

    [220425] 새싹 심기 19일차

--- num1977.py
+++ num1977.py
@@ -1,22 +1,22 @@
+# 완전제곱수 진행 중
+# 제출만 하면 됨!
+
 M = int(input())
 N = int(input())
 
-answer = 0
-answer02 = 0
-if M != 1 and int(M**(1/2)) == int(N**(1/2)):
-    print(-1)
-    exit()
-elif M ==1 and N ==1:
-    answer = 1
-    answer02 = 1
-else:
+def makeSqList(M, N):
+    sqList = []
+    if int(M**(1/2))**2 == M:
+        sqList.append(M)
+    if int(M**(1/2)) == int(N**(1/2)):
+        return sqList
     for i in range(int(M**(1/2))+1, int(N**(1/2))+1):
-        if M==1:
-            answer02=1
-            answer += 1
-        elif i == int(M**(1/2))+1:
-            answer02 = i**2
-        answer += i**2
+        sqList.append(i**2)
+    return sqList
 
-print(answer)
-print(answer02)
+result = makeSqList(M, N)
+if result:
+    print(sum(result))
+    print(min(result))
+else:
+    print(-1)
